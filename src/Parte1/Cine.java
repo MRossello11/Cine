afc65ff7452f842358deaf13f549a2de8c6b3d21
MRossello11 <mrossello11@nigul.cide.es>
2021-12-09 16:23:58 +0100

    Añadido un control de tiempo y comentarios.

--- src/Parte1/Cine.java
+++ src/Parte1/Cine.java
@@ -1,3 +1,8 @@
+/**
+ * @author MRossello11
+ * @version 1.3
+ * @since 02/12/2021
+ * @description clase de los objetos Cine*/
 package Parte1;
 
 import java.util.Random;
@@ -15,33 +20,35 @@ public class Cine {
 
     //constructor
     public Cine(String nombre, int numAsientos){
-        this.nombre = nombre;
-        this.numAsientos = numAsientos;
-        this.numFilas = crearFilas();
-        this.numColumnas = crearColumnas();
-        this.arrayAsientos = new char[numFilas][numColumnas];
+        this.nombre = nombre; //nombre del cine
+        this.numAsientos = numAsientos; //numero de asientos del cine
+        this.numFilas = crearFilas(); //filas
+        this.numColumnas = crearColumnas(); //columnas
+        this.arrayAsientos = new char[numFilas][numColumnas]; //array de los asientos
         this.copiaArrayAsientos = this.arrayAsientos.clone();
     }
 
+    //crea un numero de filas aleatorias a partir del numero de asientos
     public int crearFilas(){
-        this.numFilas = numAsientos/rand.nextInt(1, numAsientos); //asignar #filas
+        this.numFilas = numAsientos/rand.nextInt(1, numAsientos); //asignar numero filas
         return numFilas;
     }
 
+    //crea el numero de columnas a partir del numero de filas y el numero total de asientos
     public int crearColumnas(){
+        //si no hay resto (la sala es un cuadrado/rectangulo) perfecto
         if (this.numAsientos%numFilas == 0){
             numColumnas = this.numAsientos/this.numFilas;
-        }else{
+
+        }else{ //en caso de que la ultima columna no tenga el mismo numero de asientos que las demas
             numColumnas = this.numAsientos/this.numFilas + 1;
         }
 
         return numColumnas;
     }
 
+    //llena el arrayAsientos
     public void crearAsientos(){
-        System.out.println("Filas: " + this.numFilas);
-        System.out.println("Columnas: " + this.numColumnas);
-
         //resto indica el numero de filas que hay en la ultima columna
         int resto = (this.numColumnas*this.numFilas)-this.numAsientos;
 
@@ -62,9 +69,11 @@ public class Cine {
         //this.imprimirTodoArray(); //debug
     }
 
+    /*metodo que se llama desde el metodo sentar() al encontrar un asiento vacio y
+    se asigna ese asiento como ocupado
+    @since 03/12/2021*/
     public void asignarAsiento(int f, int c){
         this.arrayAsientos[f][c] = 'x';
-
     }
 
     /*
@@ -75,18 +84,33 @@ public class Cine {
 
         boolean sentado = false;
 
-        while (!sentado) {
+        while (!sentado) { //se itera hasta que se consiga asignar un asiento
 
             //numero de butaca random
             int fil = rand.nextInt(0, this.getNumFilas());
             int col = rand.nextInt(0, this.getNumColumnas());
-            numAleatorios++;
+            numAleatorios++; //aumenta el contador de numeros aleatorios generados(solo para medir eficiencia)
+
+            //evaluacion de si el asiento esta vacio
             if (this.getArrayAsientos()[fil][col] == '-') {
+                //si el asiento esta libre ('-'), se asigna como ocupado y se sale del bucle
                 this.asignarAsiento(fil, col);
                 sentado = true;
             }
+            //si no se consigue asignar el asiento, se vuelve a intentar en la proxima iteracion
         }
     }
+
+    //metodo que imprime el array de asientos
+    public void imprimirTodoArray(){
+        for (int i=0; i<this.getNumFilas(); i++){
+            for (int j=0; j<this.getNumColumnas();j++){
+                System.out.print(this.getArrayAsientos()[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    //metodos getters y setters
 
     public String getNombre() {
         return nombre;
@@ -126,14 +150,5 @@ public class Cine {
 
     public void setArrayAsientos(char[][] arrayAsientos) {
         this.arrayAsientos = arrayAsientos;
-    }
-
-    public void imprimirTodoArray(){
-        for (int i=0; i<this.getNumFilas(); i++){
-            for (int j=0; j<this.getNumColumnas();j++){
-                System.out.print(this.getArrayAsientos()[i][j]);
-            }
-            System.out.println();
-        }
     }
 }
--- src/Parte1/Main.java
+++ src/Parte1/Main.java
@@ -1,25 +1,33 @@
-package Parte1;
+/**
+ * @author MRossello11
+ * @version 1.3
+ * @since 02/12/2021
+ * @description clase principal del proyecto Cine*/
 
-import java.util.Random;
+package Parte1;
 
 public class Main {
     public static void main(String[] args) {
-        Random rand = new Random();
-        Cine c1 = new Cine("Ocimax", 2000); //crear objeto c1
+        long inicio = System.currentTimeMillis(); //tiempo inicio
+        Cine c1 = new Cine("Ocimax", 200); //crear objeto c1
         System.out.println("Nombre: " + c1.getNombre()); //mostrar nombre c1
         System.out.println("Asientos: " + c1.getNumAsientos()); //mostrar asientos c1
         c1.crearAsientos(); //creacion asientos
 
-        int contador = 0; //para evaluar eficiencia
-        int asientosOcupados = 0;
+        int asientosOcupados = 0; //contador de asientos que se han asignado
+
         do {
-            contador++;
-            c1.sentar();
-            asientosOcupados++;
-            System.out.println("..............................................");
-            c1.imprimirTodoArray();
-        } while (asientosOcupados != c1.getNumAsientos());
-        System.out.println("Num iteraciones: " + contador);
-        System.out.println("Numeros aleatorios generados: " + c1.numAleatorios);
+            c1.sentar(); //se asigna un asiento
+            asientosOcupados++; //al sentar a alguien, se aumenta el numero de asientos ocupados
+            System.out.println("..................................................."); //separa visualmente la impresion de las salas
+            c1.imprimirTodoArray(); //imprime la sala (asientos vacios y ocupados)
+        } while (asientosOcupados != c1.getNumAsientos()); //cuando se hayan ocupado tantos asientos como asientos haya, sale del bucle
+
+        //medicion de eficiencia
+        System.out.println("Numeros aleatorios generados: " + c1.numAleatorios); //mostrar cuantos numeros aleatorios se han generado
+        long fin = System.currentTimeMillis(); //tiempo final
+        double tiempoEjecucion = fin-inicio; //tiempo de ejecucion
+        System.out.println("Tiempo ejecucion: " + tiempoEjecucion + " milisegundos");
     }
+    //Fin programa
 }
